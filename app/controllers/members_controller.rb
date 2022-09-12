80345fab7ea72206bba7764ac373ad4bf9f27f17
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-12 20:09:18 +0200

    add methods in membersController

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,47 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!, only: %i[show]
+  before_action :authenticate_user!, only: %i[show_me, update_me]
 
 
-  def show
+  def show_me
     user = get_user_from_token
-    render json: {
-      message: "If you see this, you're in!",
-      user: user
-    }
+    render json: user.as_json(include: :hosted_meals)
+  end
+
+  def guested_meals
+    user = get_user_from_token
+    @meals = Attendance.where(guest_id: User.find(user.id))
+    render json: @meals.as_json(include: :meal)
+  end
+
+  def update_me
+    user = get_user_from_token
+    if user.update(user_params)
+        render json: user
+    else
+        render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @users = User.all
+    render json: @users.map{|user|
+        user.as_json(include: :hosted_meals)
+        }
+  end
+
+
+  def show_user
+    @user = User.find(params[:id])
+    render json: @user.as_json(include: :hosted_meals)
   end
 
   private
 
+    def user_params
+      params.require(:user).permit(:name, :email, :age, :description, :city, :gender)
+    end
+
   def get_user_from_token
-    
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], ENV["JWT_SECRET_KEY"]).first
     user_id = jwt_payload['sub']
     User.find(user_id.to_s)
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
                sessions: 'users/sessions',
                registrations: 'users/registrations'
              }
-  get '/member-data', to: 'members#show'
+  get '/me', to: 'members#show_me'
+  get '/guested_meals', to: 'members#guested_meals'
+  put '/update_me', to: 'members#update_me'
+  get '/users_list', to: 'members#index'
+  get '/user_detail/:id', to: 'members#show_user'
+  # post 'add_avatar', to:'members#add_avatar'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
