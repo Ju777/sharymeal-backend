376a485d942be87c9f53f55bcb3a75968d999978
Ju777 <julien.tamil@gmail.com>
2022-09-13 19:56:39 +0200

    Add Single Image for User, try to add Image for Meals.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,4 +6,13 @@ class Meal < ApplicationRecord
 
    has_many :joinCategoryMeals
    has_many :categories, through: :joinCategoryMeals
+
+   has_many_attached :images
+
+   def image_url
+      # puts "#"*50
+      # puts 'images => ', images.count
+      # puts "#"*50
+      Rails.application.routes.url_helpers.url_for(images) if images.attached?
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   put '/update_me', to: 'members#update_me'
   get '/users_list', to: 'members#index'
   get '/user_detail/:id', to: 'members#show_user'
+  # put '/update_meal/:id', to:"meals#update_meal"
   # post 'add_avatar', to:'members#add_avatar'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
