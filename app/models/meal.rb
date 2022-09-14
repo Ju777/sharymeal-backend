675411d2257dd40f3eae251660c65704b10e6915
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-14 10:10:37 +0200

    fix email validation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,12 +6,11 @@ class Meal < ApplicationRecord
    has_many :categories, through: :joinCategoryMeals
    has_many_attached :images
 
-   validates :starting_date, comparison: { greater_than: Date.today }, presence: true
+   validates :starting_date, comparison: { greater_than: Date.yesterday }, presence: true
    validates :description, length: { maximum: 500 }, presence: true
    validates :guest_capacity, numericality: { only_integer: true }, comparison: { greater_than: 0, less_than: 12 }, presence: true
-   validates :title, length: { maximum: 16, minimum: 4 }, presence: true
-   validates :price, numericality: { only_integer: true }, comparison: { greater_than: 0, less_than: 25 }, presence: true
-   validates :image_urls, length: { minimum: 2}
+   validates :title, length: { maximum: 36, minimum: 1 }, presence: true
+   validates :price, comparison: { greater_than: 0, less_than: 25 }, presence: true
 
    def image_urls
      images.map{|p| Rails.application.routes.url_helpers.url_for(p) }
