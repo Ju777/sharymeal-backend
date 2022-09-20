177539acda532cbef484595aabd3b195f2bc0d1d
Ju777 <julien.tamil@gmail.com>
2022-09-20 10:47:44 +0200

    Debug members-data line 6.

--- db/migrate/20220912103435_attendances.rb
+++ db/migrate/20220912103435_attendances.rb
@@ -1,10 +1,10 @@
 class Attendances < ActiveRecord::Migration[7.0]
     def change
       create_table :attendances do |t|
-        # t.references :user, foreign_key: true
-        # t.references :meal, foreign_key: true
-        t.belongs_to :user, index: true
-        t.belongs_to :meal, index: true
+        t.references :user, index: true, foreign_key: {to_table: :users}
+        t.references :meal, index: true, foreign_key: {to_table: :meals }
+        # t.belongs_to :user, index: true
+        # t.belongs_to :meal, index: true
         t.timestamps
       end
     end
