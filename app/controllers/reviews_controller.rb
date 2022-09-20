db8ff72e0a61c51639f06bf4cd3894eaeac1c6af
Joff317 <j.freixanet@hotmail.com>
2022-09-20 19:41:02 +0200

    add host_id to reviews params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -64,6 +64,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.require(:review).permit(:content, :rating, :host)
+      params.require(:review).permit(:content, :rating, :host_id)
     end
 end
