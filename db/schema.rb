b98568de5c5038fa0b5160ae4b0069c71fec9976
Joff317 <j.freixanet@hotmail.com>
2022-09-22 18:15:44 +0200

    price meal in integer

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_143541) do
   create_table "meals", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price"
+    t.integer "price"
     t.integer "guest_capacity"
     t.integer "guest_registered"
     t.date "starting_date"
@@ -135,6 +135,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_143541) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "attendances", "meals"
+  add_foreign_key "attendances", "users"
   add_foreign_key "meals", "users", column: "host_id"
   add_foreign_key "messages", "users", column: "recipient_id"
   add_foreign_key "messages", "users", column: "sender_id"
