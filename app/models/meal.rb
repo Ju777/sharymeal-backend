3a6e2941dd97a8f211f6d1ca2caedc47c1bd3225
Ju777 <julien.tamil@gmail.com>
2022-09-22 15:02:17 +0200

    Disable validations for testing meals with past dates.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -7,11 +7,11 @@ class Meal < ApplicationRecord
    has_many :categories, through: :joinCategoryMeals
    has_many_attached :images
 
-   validates :starting_date, comparison: { greater_than: Date.yesterday }, presence: true
-   validates :description, length: { maximum: 500 }, presence: true
-   validates :guest_capacity, numericality: { only_integer: true }, comparison: { greater_than: 0, less_than: 12 }, presence: true
-   validates :title, length: { maximum: 50, minimum: 1 }, presence: true
-   validates :price, comparison: { greater_than: 0, less_than: 25 }, presence: true
+  #  validates :starting_date, comparison: { greater_than: Date.yesterday }, presence: true
+  #  validates :description, length: { maximum: 500 }, presence: true
+  #  validates :guest_capacity, numericality: { only_integer: true }, comparison: { greater_than: 0, less_than: 12 }, presence: true
+  #  validates :title, length: { maximum: 50, minimum: 1 }, presence: true
+  #  validates :price, comparison: { greater_than: 0, less_than: 25 }, presence: true
 
    def image_urls
      images.map{|image| Rails.application.routes.url_helpers.url_for(image) } if images.attached?
